8f09a90a37893802d02d13c97170ca7e6c3ca071
ryo <ryo7000@gmail.com>
2021-09-04 22:07:49 +0900

    Use SFC and tsx

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+<script lang="tsx">
+import { defineComponent } from 'vue';
+import logo from './assets/logo.png'
 import HelloWorld from './components/HelloWorld.vue'
-</script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-</template>
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return () => (
+      <>
+        <img alt="Vue logo" src={logo} />
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      </>
+    )
+  }
+})
+</script>
 
 <style>
 #app {
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,37 +1,45 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="tsx">
+import { ref, defineComponent } from 'vue'
 
-defineProps<{ msg: string }>()
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  setup(props) {
+    const count = ref(0)
 
-const count = ref(0)
+    return () => (
+      <>
+        <h1>{ props.msg }</h1>
+
+        <p>
+          Recommended IDE setup:
+          <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+          +
+          <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+        </p>
+
+        <p>See <code>README.md</code> for more information.</p>
+
+        <p>
+          <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Docs
+          </a>
+          |
+          <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+        </p>
+
+        <button type="button" onClick={() => count.value++}>count is: { count.value }</button>
+        <p>
+          Edit
+          <code>components/HelloWorld.vue</code> to test hot module replacement.
+        </p>
+      </>
+    );
+  },
+});
 </script>
-
-<template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-</template>
 
 <style scoped>
 a {

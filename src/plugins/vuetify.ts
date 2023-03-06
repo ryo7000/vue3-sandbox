ced4b7f067fc6d44e4cbda4ef13207fe8faf8ffc
ryo <ryo7000@gmail.com>
2023-03-06 21:48:58 +0900

    Add material-design-icons-iconfont

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,4 +1,14 @@
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, md } from 'vuetify/iconsets/md'
 
-export default createVuetify()
+export default createVuetify({
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md
+    }
+  }
+})
